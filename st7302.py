629ccdae9d18a5fc3df6cc30f7d122b1221bb21f
0ut4t1m3 <mikefoxme@gmail.com>
2024-09-09 18:30:36 +0930

    Update st7302.py
    Add boilerplate

--- st7302.py
+++ st7302.py
@@ -1,3 +1,19 @@
+#MPY_ST7302 - ST7302 driver for Micropython
+#Copyright (C) 2024 0ut4t1m3
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import time, framebuf, gc
 
 class ST7302(framebuf.FrameBuffer):

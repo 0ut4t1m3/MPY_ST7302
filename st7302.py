e0d9d15a721c31585b95216ebfc53ec439b5e2ff
Mike . <mikefoxme@gmail.com>
2026-03-06 16:49:46 +0930

    Adding improved code for the draw function, stripping pin hold and removing unneded commands from init function.

--- st7302.py
+++ st7302.py
@@ -1,23 +1,8 @@
-#MPY_ST7302 - ST7302 driver for Micropython
-#Copyright (C) 2024 0ut4t1m3
-
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
-
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-
-#You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 import time, framebuf, gc
+from machine import Pin
 
 class ST7302(framebuf.FrameBuffer):
-    
+
     FPS0_25 = const(0x00)
     FPS0_5  = const(0x01)
     FPS1    = const(0x02)
@@ -27,35 +12,24 @@ class ST7302(framebuf.FrameBuffer):
 
     def __init__(self, spi, cs, dc, rst, fps=FPS8, init=True):
         self.spi = spi
-        self.rest_pin = rst
-        self.dc_pin = dc
-        self.cs_pin = cs
-        self.cs_pin.init(self.cs_pin.OUT, value=1)
-        self.dc_pin.init(self.dc_pin.OUT)
-        self.rest_pin.init(self.rest_pin.OUT)
+        self.rest_pin = Pin(rst, Pin.OUT)
+        self.dc_pin = Pin(dc, Pin.OUT)
+        self.cs_pin = Pin(cs, Pin.OUT, value=1)
         self.fps = fps
         self.buffer  = bytearray(4125)
         self.out_buf = bytearray(4125)
         super().__init__(self.buffer, 250, 122, framebuf.MONO_HLSB)
-        self.pinlock(False)
         if init: self.init()
         
     def invert(self, val=True):
         self.command(0x21 if val else 0x20)
         time.sleep_ms(100)
         
-    def pinlock(self, enabled):
-        self.rest_pin.init(hold=enabled)
-        self.dc_pin.init(hold=enabled)
-        self.cs_pin.init(hold=enabled)
-        
     def sleep(self):
         self.command(0x10)
         time.sleep_ms(10)
-        self.pinlock(True)
         
     def wake(self):
-        self.pinlock(False)
         self.command(0x11)
         time.sleep_ms(10)
     
@@ -67,8 +41,7 @@ class ST7302(framebuf.FrameBuffer):
     def clear(self):
         self.fill(0)
         self.draw()
-        
-    @micropython.native
+    
     def command(self, command, data=None):
         self.cs_pin.value(0)
         self.dc_pin.value(0)
@@ -80,55 +53,56 @@ class ST7302(framebuf.FrameBuffer):
     
     @micropython.viper
     def draw(self):
+        src = ptr8(self.buffer)
+        dst = ptr8(self.out_buf)
         for col in range(125):
             blk = 0
             byt_msk = (col*2) // 8
             bit_msk = 7 - (col*2) % 8
+            mask_a = 1 <<  bit_msk
+            mask_b = 1 << (bit_msk-1)
             for l in range(col*33, col*33+32, 1):
-                buf = 0
-                buf = buf | (0b10000000 if int(self.buffer[byt_msk +  blk])     & (1 <<  bit_msk  )  else 0)
-                buf = buf | (0b01000000 if int(self.buffer[byt_msk +  blk])     & (1 << (bit_msk-1)) else 0)           
-                buf = buf | (0b00100000 if int(self.buffer[byt_msk + (blk+32)]) & (1 <<  bit_msk  )  else 0)
-                buf = buf | (0b00010000 if int(self.buffer[byt_msk + (blk+32)]) & (1 << (bit_msk-1)) else 0)
-                buf = buf | (0b00001000 if int(self.buffer[byt_msk + (blk+64)]) & (1 <<  bit_msk  )  else 0)
-                buf = buf | (0b00000100 if int(self.buffer[byt_msk + (blk+64)]) & (1 << (bit_msk-1)) else 0)
-                buf = buf | (0b00000010 if int(self.buffer[byt_msk + (blk+96)]) & (1 <<  bit_msk  )  else 0)
-                buf = buf | (0b00000001 if int(self.buffer[byt_msk + (blk+96)]) & (1 << (bit_msk-1)) else 0)
-                self.out_buf[l] = buf
+                b0 = src[byt_msk +  blk]
+                b1 = src[byt_msk + (blk+32)]
+                b2 = src[byt_msk + (blk+64)]
+                b3 = src[byt_msk + (blk+96)]
+                res = 0
+                if b0 & mask_a: res |= 0x80
+                if b0 & mask_b: res |= 0x40
+                if b1 & mask_a: res |= 0x20
+                if b1 & mask_b: res |= 0x10
+                if b2 & mask_a: res |= 0x08
+                if b2 & mask_b: res |= 0x04
+                if b3 & mask_a: res |= 0x02
+                if b3 & mask_b: res |= 0x01
+                dst[l] = res
                 blk += 128 
         self.command(0x2c, self.out_buf)
-        gc.collect()
 
     def init(self):
         self.reset()
-        self.command(0x38)
-        self.command(0xeb, b'\x02')
-        self.command(0xd7, b'\x68')
-        self.command(0xd1, b'\x01')
-        self.command(0xc0, b'\x80')
-        self.command(0xc1, b'\x28\x28\x28\x28\x14\x00')
-        self.command(0xc2, b'\x00\x00\x00\x00')
-        self.command(0xcb, b'\x14')
-        self.command(0xb4, b'\xe5\x77\xf1\xff\xff\x4f\xf1\xff\xff\x4f')
-        self.command(0xb0, b'\x64')
-        self.command(0xb2, bytearray([0x00,self.fps]))
-        self.command(0x11)
+        self.command(0xeb, b'\x02') #NV Load Enable
+        self.command(0xd7, b'\x68') #NV Load Ctrl
+        self.command(0xd1, b'\x01') #Booster Enable
+        self.command(0xc0, b'\x80') #Gate Control
+        self.command(0xc1, b'\x28\x28\x28\x28\x14\x00') #Source High Voltage Control
+        self.command(0xc2, b'\x00\x00\x00\x00') #Source Low Voltage Control
+        self.command(0xcb, b'\x14') #VCOMH Control
+        self.command(0xb4, b'\xe5\x77\xf1\xff\xff\x4f\xf1\xff\xff\x4f') #Update Period Gate EQ Control
+        self.command(0xb0, b'\x64') #Duty Setting
+        self.command(0x39) #Low power mode
+        self.command(0xb2, bytearray([0x00,self.fps])) #FR Control
+        self.command(0x11) #Sleep out
         time.sleep_ms(10)
-        self.command(0xc7, b'\xa6\xe9')
-        self.command(0x34)
-        self.command(0x36, b'\x00')
-        self.command(0x3a, b'\x11')
-        self.command(0xb9, b'\x23')
-        self.command(0xb8, b'\x09')
-        self.command(0xd0, b'\x1f')
-        self.command(0x29)
-        self.command(0xb9, b'\xe3')
-        time.sleep_ms(100)
-        self.command(0xb9, b'\x23')
-        self.command(0x72, b'\x00')
-        self.command(0x39)
-        self.command(0x19, b'\x23')
-        self.command(0x2a, b'\x19\x23')
-        self.command(0x2b, b'\x00\x7c')
-        self.command(0xb3, b'\x94')
+        self.command(0xc7, b'\xa6\xe9') #OSC Enable
+        self.command(0x34) #Tearing effect line off
+        self.command(0x36, b'\x00') #Memory data access control
+        self.command(0x3a, b'\x11') #Data format select
+        self.command(0xb9, b'\x23') #Source Setting
+        self.command(0xb8, b'\x09') #Panel Setting
+        self.command(0x29) #Display on
+        self.command(0x2a, b'\x19\x23') #Column address set
+        self.command(0x2b, b'\x00\x7c') #Row address set
+        self.command(0xb3, b'\x94') #VCOM EQ Enable
         self.clear()
+        
